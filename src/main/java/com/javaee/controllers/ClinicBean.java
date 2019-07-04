d88229e550a605da90fc794efb907bd237afc77c
michalniedziela <michal.niedziela@ic-consult.com>
2019-07-04 18:21:08 +0100

    show clinics

--- src/main/java/com/javaee/controllers/ClinicBean.java
+++ src/main/java/com/javaee/controllers/ClinicBean.java
@@ -4,7 +4,12 @@ package com.javaee.controllers;
 import com.javaee.config.DBManager;
 import com.javaee.domain.Clinic;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class ClinicBean {
 
@@ -27,8 +32,45 @@ public class ClinicBean {
         clinic.setId(null);
         em.persist(clinic);
         em.getTransaction().commit();
+        addInformation("Added clinic");
         em.close();
         this.clinic = new Clinic();
         return null;
+    }
+
+    public List<Clinic> getList() {
+        EntityManager em = DBManager.getManager().createEntityManager();
+        List list = em.createNamedQuery("Clinic.findAll").getResultList();
+        em.close();
+        return list;
+    }
+
+    public void clinicListener(ActionEvent ae) {
+        String ids = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("clinicID").toString();
+        int id = Integer.parseInt(ids);
+        this.clinic.setId(id);
+    }
+
+    public String loadToEdit() {
+        EntityManager em = DBManager.getManager().createEntityManager();
+        this.clinic = em.find(Clinic.class, clinic.getId());
+        em.close();
+        return "/editclinic.xhtml";
+    }
+
+    public String remove() {
+        EntityManager em = DBManager.getManager().createEntityManager();
+        em.getTransaction().begin();
+        this.clinic = em.find(Clinic.class, clinic.getId());
+        em.remove(this.clinic);
+        this.clinic = new Clinic();
+        em.getTransaction().commit();
+        em.close();
+        this.addInformation("Removed clinic");
+        return null;
+    }
+
+    public void addInformation(String s) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, s, ""));
     }
 }
